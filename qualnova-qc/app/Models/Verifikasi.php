ea58f0f9135fb578073db426e4f9fffe547f23c3
Fazrilukman <fajrilukman194@gmail.com>
2025-11-26 18:55:38 +0700

    nambahin model Verifikasi. disesuain sama struktur tabel dan kebutuhan fitur verifikasi

---
+++ qualnova-qc/app/Models/Verifikasi.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Verifikasi extends Model
+{
+    use HasFactory;
+
+    protected $table = 'verifikasi';
+    protected $primaryKey = 'id_verifikasi';
+
+    protected $fillable = [
+        'id_cacat',
+        'qc_id',
+        'tanggal_verifikasi',
+        'valid',
+    ];
+
+    // 🔗 Relasi
+    public function qc()
+    {
+        return $this->belongsTo(User::class, 'qc_id', 'id');
+    }
+    
+    public function dataCacat()
+    {
+        return $this->belongsTo(DataCacat::class, 'id_cacat', 'id_cacat');
+    }
+    
+}
